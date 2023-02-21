6a3d48f03a7bb63a77aaaf3f9cfbb90bfc0c3e8d
gali-yuwan-kuanmian <125937042+gali-yuwan-kuanmian@users.noreply.github.com>
2023-02-20 18:53:04 -0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -34,7 +34,7 @@ const CONFIG = {
 
   // notion configuration (required)
   notionConfig: {
-    pageId: process.env.356fa18cc4e844c29e824f56a91c27a4,
+    pageId:356fa18cc4e844c29e824f56a91c27a4,
   },
 
   // plugin configuration (optional)
