c8b0abcb319e1a48915d240f807fa3b95c653936
gali-yuwan-kuanmian <125937042+gali-yuwan-kuanmian@users.noreply.github.com>
2023-03-04 22:30:29 -0800

    Update Scripts.tsx

--- src/components/Scripts.tsx
+++ src/components/Scripts.tsx
@@ -8,7 +8,7 @@ const Scripts: React.FC = () => (
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${CONFIG.googleAnalytics.config.measurementId}`}
         />
-        <Script strategy="lazyOnload" id="ga">
+        <Script>
           {`window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
